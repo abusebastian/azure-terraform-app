54f0c17350337d273664d4f19fd0738816f2f243
Abu Sebastian <abusebastian@MBP16Abebastian.home>
2024-07-29 00:33:03 +0200

    final

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,6 @@ output "container_app_ingress" {
     external_enabled = azurerm_container_app.mycontainerapp.ingress[0].external_enabled
     target_port      = azurerm_container_app.mycontainerapp.ingress[0].target_port
     traffic_weight   = azurerm_container_app.mycontainerapp.ingress[0].traffic_weight
-    endpoint_url     = azurerm_container_app.mycontainerapp.default_hostname
+    endpoint_url     = "https://${azurerm_container_app.mycontainerapp.name}--${azurerm_container_app.mycontainerapp.location}.azurecontainerapps.io"
   }
 }
