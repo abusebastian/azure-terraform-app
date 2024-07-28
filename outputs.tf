e815cfb4833c7ddeaefc944a4706135a1402f437
Abu Sebastian <abusebastian@MBP16Abebastian.home>
2024-07-28 23:59:31 +0200

    final

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "container_app_ingress" {
   value = {
-    external_enabled = azurerm_container_app.mycontainerapp.ingress.external_enabled
-    target_port      = azurerm_container_app.mycontainerapp.ingress.target_port
-    traffic_weight   = azurerm_container_app.mycontainerapp.ingress.traffic_weight
+    external_enabled = azurerm_container_app.mycontainerapp.ingress[0].external_enabled
+    target_port      = azurerm_container_app.mycontainerapp.ingress[0].target_port
+    traffic_weight   = azurerm_container_app.mycontainerapp.ingress[0].traffic_weight
   }
 }
